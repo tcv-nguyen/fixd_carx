845c37474df768aea2fdd24a8022c65b2f8b08ad
Anthony Nguyen <tony@digsite.com>
2022-08-06 12:28:44 -0500

    Refactor api post spec

--- spec/api/v1/post_spec.rb
+++ spec/api/v1/post_spec.rb
@@ -2,23 +2,16 @@ require 'rails_helper'
 
 describe 'Post API', type: :request do
   let(:user) { create(:user, with_api_token: true) }
-  let(:post_params) { { title: 'Post title', body: 'This is post content' } }
-  let(:valid_params) { post_params.merge(token: user.api_token) }
+  let(:valid_params) do
+    {
+      token: user.api_token,
+      title: 'Post title',
+      body: 'This is post content'
+    }
+  end
 
   before do
     post '/api/v1/post', params: params
-  end
-
-  describe 'create new Post without API token' do
-    let(:params) { post_params }
-
-    it 'should return status 401' do
-      expect(response.status).to eq(401)
-    end
-
-    it 'should return invalid API token message' do
-      expect(response_message).to eq('Invalid API Token')
-    end
   end
 
   describe 'create new Post with valid token' do
@@ -34,6 +27,18 @@ describe 'Post API', type: :request do
   end
 
   describe 'create new Post with missing data' do
+    describe 'without API token' do
+      let(:params) { valid_params.except(:token) }
+  
+      it 'should return status 401' do
+        expect(response.status).to eq(401)
+      end
+  
+      it 'should return invalid API token message' do
+        expect(response_message).to eq('Invalid API Token')
+      end
+    end
+
     describe 'with missing title' do
       let(:params) { valid_params.except(:title) }
 
